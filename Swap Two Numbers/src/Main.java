fbf91ebcf9c283f5b2225783194d759d265d0512
yogesh <96315347+yogeshrathee@users.noreply.github.com>
2023-09-30 16:13:19 +0530

    Add files via upload

---
+++ Swap Two Numbers/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the value of a :: ");
+        int a=sc.nextInt();
+        System.out.print("enter the value of b :: ");
+        int b=sc.nextInt();
+        System.out.println("Before swap the two number , value of  'a' is "+a+" and 'b' is "+b);
+        swapNum.SwapNum( a,  b);
+    }
+}
---
+++ Swap Two Numbers/src/swapNum.java
@@ -0,0 +1,9 @@
+public class swapNum {
+    public static void SwapNum(int a, int b){
+        int temp = a;
+        a=b;
+        b=temp;
+        System.out.println("After swap the two number , value of  'a' is "+a+" and 'b' is "+b);
+    }
+
+}
