d390efb22ccbbbed05a0a8fde3924f1bcb8604b5
yogesh <96315347+yogeshrathee@users.noreply.github.com>
2023-09-30 10:03:39 +0530

    Add files via upload

---
+++ Check Whether a Number is Positive or Negative/src/PositiveNegative.java
@@ -0,0 +1,13 @@
+public class PositiveNegative {
+    public static void pos_neg(int n){
+        if(n > 0){
+            System.out.println(n+ " is Positive number");
+        }
+        else if(n < 0){
+            System.out.println(n+ " is negative number");
+        }
+        else{
+            System.out.println(n+ " is not belong to negative or positive number !!!");
+        }
+    }
+}
